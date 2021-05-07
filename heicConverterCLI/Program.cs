b37a7e37ddf7431b4b5eb68bb42772606fabc1df
zotya701 <zotya701@gmail.com>
2021-05-08 00:19:47 +0200

    Make heicConverter a "Windows Application" (no console window) Create separate heicConverterCLI project just for the console window Now converting .heic to jpg / png / bmp / tiff will not open a console window

---
+++ heicConverterCLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace heicConverterCLI
+{
+  public class Program
+  {
+    public static async Task<int> Main(string[] args)
+    {
+      return await heicConverter.Program.Main(args);
+    }
+  }
+}
